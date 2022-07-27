672c4ff5ce35ec179f04a12e513d04e1ef5d5b24
Ilia-SB <ilia.a.ivanov@gmail.com>
2022-07-27 18:50:26 +0300

    Removed all leds blinking

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "5"
+  #define BUILD_NUMBER "11"
 #endif
 #ifndef VERSION
-  #define VERSION "v0.1.220726_5 - 2022-07-26 13:05:07.553465"
+  #define VERSION "v0.1.220727_11 - 2022-07-27 15:03:55.656803"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v0.1.220726_5"
+  #define VERSION_SHORT "v0.1.220727_11"
 #endif
