ea0c52230d80119eaf9ac020d15982599197c0be
Ilia Ivanov <ilia.a.ivanov@gmail.com>
2022-10-03 14:48:52 +0300

    Fixed build-no display in settings

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "309"
+  #define BUILD_NUMBER "316"
 #endif
 #ifndef VERSION
-  #define VERSION "v1.0.221003_309 - 2022-10-03 02:21:21.011131"
+  #define VERSION "v1.0.221003_316 - 2022-10-03 14:44:35.173719"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v1.0.221003_309"
+  #define VERSION_SHORT "v1.0.221003_316"
 #endif
