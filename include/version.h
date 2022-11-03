6d943c8a7aa6fbb734daed4a2e8b2cd5a63b4c06
Ilia-SB <ilia.a.ivanov@gmail.com>
2022-11-03 18:13:00 +0300

    1.4 Version change

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "369"
+  #define BUILD_NUMBER "370"
 #endif
 #ifndef VERSION
-  #define VERSION "v1.4.221103_369 - 2022-11-03 17:48:25.137514"
+  #define VERSION "v1.4.221103_370 - 2022-11-03 18:11:37.525894"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v1.4.221103_369"
+  #define VERSION_SHORT "v1.4.221103_370"
 #endif
