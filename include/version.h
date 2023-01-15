c732d1f7aee58ab021cc6057f1d4c0b6bd91580d
Ilia Ivanov <ilia.a.ivanov@gmail.com>
2023-01-15 19:18:55 +0300

    Version bump to 1.9

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "439"
+  #define BUILD_NUMBER "440"
 #endif
 #ifndef VERSION
-  #define VERSION "v1.8.230115_439 - 2023-01-15 19:14:17.006370"
+  #define VERSION "v1.9.230115_440 - 2023-01-15 19:17:45.766995"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v1.8.230115_439"
+  #define VERSION_SHORT "v1.9.230115_440"
 #endif
