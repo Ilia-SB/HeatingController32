520e9f619e4f2b3dc03cfdf06610c9a3c2e10b07
Ilia Ivanov <ilia.a.ivanov@gmail.com>
2023-03-09 15:01:40 +0300

    1.10: Added external temp sensors

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "444"
+  #define BUILD_NUMBER "445"
 #endif
 #ifndef VERSION
-  #define VERSION "v1.10.230309_444 - 2023-03-09 14:58:57.655414"
+  #define VERSION "v1.10.230309_445 - 2023-03-09 15:01:12.341769"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v1.10.230309_444"
+  #define VERSION_SHORT "v1.10.230309_445"
 #endif
