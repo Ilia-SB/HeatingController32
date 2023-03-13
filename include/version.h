a8c60c067b677d57b9700e8d4841c5fdc638a69e
Ilia Ivanov <ilia.a.ivanov@gmail.com>
2023-03-13 16:16:12 +0300

    1.11 Changed running median to

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "456"
+  #define BUILD_NUMBER "457"
 #endif
 #ifndef VERSION
-  #define VERSION "v1.10.230313_456 - 2023-03-13 15:11:29.881588"
+  #define VERSION "v1.11.230313_457 - 2023-03-13 16:09:13.469345"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v1.10.230313_456"
+  #define VERSION_SHORT "v1.11.230313_457"
 #endif
