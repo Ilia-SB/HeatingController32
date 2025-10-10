e68617aeb0a8342eaa4bb8b0b3bbff1a0c3da4f7
Ilia-SB <ilia.a.ivanov@gmail.com>
2025-10-10 21:17:27 +0300

    .gitignore

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "560"
+  #define BUILD_NUMBER "564"
 #endif
 #ifndef VERSION
-  #define VERSION "ver. 2.0.251010_560 - 2025-10-10 21:08:19.733407"
+  #define VERSION "ver. 2.0.251010_564 - 2025-10-10 21:17:07.704130"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "ver. 2.0.251010_560"
+  #define VERSION_SHORT "ver. 2.0.251010_564"
 #endif
