38f53bdf8ce1b97742a75a56d3f8b4250f3d7787
Ilia-SB <ilia.a.ivanov@gmail.com>
2025-10-11 00:56:35 +0300

    Version bump to 2.3

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "583"
+  #define BUILD_NUMBER "584"
 #endif
 #ifndef VERSION
-  #define VERSION "ver. 2.2.251011_583 - 2025-10-11 00:50:23.734724"
+  #define VERSION "ver. 2.3.251011_584 - 2025-10-11 00:56:13.491296"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "ver. 2.2.251011_583"
+  #define VERSION_SHORT "ver. 2.3.251011_584"
 #endif
