c9834f839c04a75731b14e3ab8fe098bf0df6786
Ilia-SB <ilia.a.ivanov@gmail.com>
2025-10-15 23:14:43 +0300

    Adjusted debug page styles for improved readability.

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "672"
+  #define BUILD_NUMBER "676"
 #endif
 #ifndef VERSION
-  #define VERSION "ver. 2.7.251015_672 - 2025-10-15 22:51:31.282717"
+  #define VERSION "ver. 2.7.251015_676 - 2025-10-15 23:13:07.402630"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "ver. 2.7.251015_672"
+  #define VERSION_SHORT "ver. 2.7.251015_676"
 #endif
